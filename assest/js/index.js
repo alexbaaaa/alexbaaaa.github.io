4f10d36b9f71b186e53ba43b217bd7bb63558728
alexbaaaa <aalejandrodesarrollo@gmail.com>
2025-12-04 01:27:11 +0100

    ajustes 2

--- assest/js/index.js
+++ assest/js/index.js
@@ -4,13 +4,29 @@ let contact = document.getElementById("contact");
 let contact_button = document.getElementById("contact_button");
 let close_button = document.getElementById("close_button");
 let open = document.getElementById("S01");
+
 contact_button.addEventListener("click", ()=>{
     Anime.curtain(contact, 0, 1);
 });
 close_button.addEventListener("click", ()=>{
     Anime.curtain(contact, 1, 0);
 });
+
 open.addEventListener("click", ()=> {
     Anime.open(open);
 })
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".back_top", {
+    opacity: 1,
+    duration: 1.5,
+    y: -50,
+    scrollTrigger: {
+        trigger: ".body",  
+        start: "top top",           
+        toggleActions: "play none none reverse"
+    }
+});
+
 
--- assest/js/modules/anime.js
+++ assest/js/modules/anime.js
@@ -1,17 +1,30 @@
-import { animate } from 'https://esm.sh/animejs';
 export class Anime {
     constructor(element) {
         this._element = element;
     }
-    static curtain(element, direction, body_display) {
-        element.style.display = ((body_display==0)? "none" : "flex");
+    static curtain(element, direction, object_display) {
         let body = document.getElementById("body");
-        body.style.display = ((body_display==1)? "none" : "block");
-        
-        animate("#"+element.id, { x: ((direction==0)? "-": " ") + '100vw' });
+        body.style.display = ((object_display==1)? "none" : "block");
+
+        const timeline = gsap.timeline();
+        timeline.to(element, {
+            x: direction== 0 ? "-100vw" : "100vw",
+            duration: 1.5,
+            ease: "power2.out"
+        })
     }
+
     static open(open) {
         console.log(open);
     }
+    static hola() {
+        gsap.to(".prueba", {
+            x: 100
+        })
+    }
+
+       
+    
+
 
 }
